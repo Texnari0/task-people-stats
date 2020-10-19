c7aa98ecea1cf943d98c2ac1bcb9328f50b23f80
Nazar Vorotsianko <texnari0>
2020-10-19 16:58:22 +0200

    Dodawanie nowej funkcji klasy PeopleStats oraz wyłowanie jej

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+
 class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        PeopleStats peopleStats = new PeopleStats(Paths.get("śćieżka", "do", "pliku"));
+
+        System.out.println(String.format("Liczba osób: %d", peopleStats.count()));
     }
 }
 
@@ -16,6 +25,9 @@ class PeopleStats {
         } catch (IOException e) {
             throw new IllegalStateException(e);
         }
+    }
+    public long count() {
+        return people.size();
     }
 }
 

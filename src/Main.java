ccbd535cfcca3f7afa2fc6840b53f902bad69cd1
Nazar Vorotsianko <texnari0>
2020-10-19 16:55:12 +0200

    Dodawanie zmiennej lastName i konfiguracja konstrukturo oraz dodawanie getter

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,26 @@
-public class Main {
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
+
+class PeopleStats {
+
+}
+
+class Person {
+    private final String firstName;
+    private final String lastName;
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
 }
